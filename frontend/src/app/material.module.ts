990aa88f39d1dff25021d9d38db9087a9a30c2f9
Łapek <joi1.512@gmail.com>
2018-01-06 18:00:02 +0100

    Added Angular Material and simple splash-screen.

---
+++ frontend/src/app/material.module.ts
@@ -0,0 +1,14 @@
+import {MatToolbarModule} from '@angular/material';
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [
+    MatToolbarModule
+  ],
+  exports: [
+    MatToolbarModule
+  ],
+})
+
+export class MaterialModule {
+}
