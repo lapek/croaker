e1c1d1bee929de915b7ef172a15d1b1a153cce2a
Lapek <joi1.512@gmail.com>
2018-07-22 20:57:44 +0200

    Update Angular from 5 to 6

--- frontend/src/app/_services/auth.service.ts
+++ frontend/src/app/_services/auth.service.ts
@@ -1,6 +1,8 @@
+
+import {map} from 'rxjs/operators';
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import 'rxjs/add/operator/map';
+
 import {TOKEN_AUTH_PASSWORD, TOKEN_AUTH_USERNAME} from './auth.constant';
 
 @Injectable()
@@ -15,13 +17,13 @@ export class AuthService {
       'Authorization': 'Basic ' + btoa(TOKEN_AUTH_USERNAME + ':' + TOKEN_AUTH_PASSWORD)
     });
 
-    return this.http.post<any>('/oauth/token', body, {headers: headers})
-      .map(user => {
+    return this.http.post<any>('/oauth/token', body, {headers: headers}).pipe(
+      map(user => {
         if (user && user.access_token) {
           localStorage.setItem('currentUser', JSON.stringify(user));
         }
         return user;
-      });
+      }));
   }
 
   logout() {
